1f49fe3e76ce8f7811b1fa3d7f18a3fa6f729f12
larrycodes <lawrenceokongo16@gmail.com>
2024-10-23 16:00:10 +0300

    Imported Dog and using Dog code.

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/LarryCodes/puppy
 
 go 1.23.2
+
+require github.com/LarryCodes/dog v0.0.0-20241023125622-1c090571f168
--- puppy.go
+++ puppy.go
@@ -1,9 +1,21 @@
 package puppy
 
+import (
+  "github.com/LarryCodes/dog"
+)
+
 func Bark() string {
   return "Woof!"
 }
 
 func Barks() string {
   return "Woof! Woof! Woof!"
+}
+
+func BigBark() string {
+  return dog.WhenGrownUp("Woof!")
+}
+
+func BigBarks() string {
+  return dog.WhenGrownUp("Woof! Woof! Woof!")
 }
